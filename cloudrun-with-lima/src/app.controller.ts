607e208b0a5afbd76756d6b2fcc03834deadd97d
y-ohgi <ohgi.211@gmail.com>
2023-07-16 23:28:20 +0900

    add endpoints

--- cloudrun-with-lima/src/app.controller.ts
+++ cloudrun-with-lima/src/app.controller.ts
@@ -3,10 +3,20 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('2')
+  getHello2(): string {
+    return 'hello2';
+  }
+
+  @Get('/3')
+  getHello3(): string {
+    return 'hello3';
   }
 }
